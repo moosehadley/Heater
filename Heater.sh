a20fa276561ba644479def7f7c863f2dd6bbba50
martin-fink <martin-fink@users.noreply.github.com>
2017-06-16 20:35:03 +0200

    Made everything more dynamic
    Create threads in a loop
    Don't kill every cat instance on the system but only created ones

--- Heater.sh
+++ Heater.sh
@@ -1,29 +1,21 @@
-#/bin/bash
+#!/bin/bash
 
 #This script will make your laptop very hot.
 #It was made for a MacBook Pro, but should work fine on other UNiX-based systems.
 #Starting 8 threads, for 8 cores.
 echo "Heating up..."
-cat /dev/zero > /dev/null&
-cat /dev/zero > /dev/null&
-cat /dev/zero > /dev/null&
-cat /dev/zero > /dev/null&
-cat /dev/zero > /dev/null&
-cat /dev/zero > /dev/null&
-cat /dev/zero > /dev/null&
-cat /dev/zero > /dev/null&
-echo "All systems go!"
-read -p "Press any key to stop... " -n1 -s
-#And will kill it 12 times to make sure it's dead.
-killall cat
-killall cat
-killall cat
-killall cat
-killall cat
-killall cat
-killall cat
-killall cat
-killall cat
-killall cat
-killall cat
-killall cat
+
+NUM_CORES=8
+PIDS=()
+
+for i in $(seq 0 $NUM_CORES); do
+  echo $i
+  cat /dev/zero > /dev/null &
+  PIDS+=($!)
+done
+
+read -p "Press any key to stop..." -n1 -s
+
+for i in "${PIDS[@]}"; do
+  kill $i
+done
