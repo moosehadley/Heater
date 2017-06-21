d55735d92b20c8319a29a4365c45e1a82aabaa70
Alexander Buzanis <alex5630@me.com>
2017-06-21 13:28:21 -0500

    Num of threads = num of cores, newline after stop

--- Heater.sh
+++ Heater.sh
@@ -2,20 +2,20 @@
 
 #This script will make your laptop very hot.
 #It was made for a MacBook Pro, but should work fine on other UNiX-based systems.
-#Starting 8 threads, for 8 cores.
 echo "Heating up..."
 
-#Subtract one core. We start counting from Zero.
-NUM_CORES=7
+#Determines number of available logical cores (hyperthreading-aware)
+NUM_CORES=$(getconf _NPROCESSORS_ONLN 2>/dev/null || getconf NPROCESSORS_ONLN 2>/dev/null || echo 1)
 PIDS=()
 
-for i in $(seq 0 $NUM_CORES); do
+for i in $(seq 1 $NUM_CORES); do
   echo $i
   cat /dev/zero > /dev/null &
   PIDS+=($!)
 done
 
 read -p "Press any key to stop..." -n1 -s
+echo
 
 for i in "${PIDS[@]}"; do
   kill $i
